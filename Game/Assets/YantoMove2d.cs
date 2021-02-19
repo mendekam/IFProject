0586026b80ced047839f79c5b6a20f77b5e00a36
maldimz <79178557+maldimz@users.noreply.github.com>
2021-02-19 20:49:24 +0700

    Update YantoMove2d.cs
    Aku tambahi ben iso kanan kiri menghadap

--- Game/Assets/YantoMove2d.cs
+++ Game/Assets/YantoMove2d.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class YantoMove2d : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    bool facingRight = true;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        FlipPlayer();
+        FlipTrigger();
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
@@ -24,5 +28,20 @@ public class YantoMove2d : MonoBehaviour
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
 
         }
+    }
+    
+    void FlipTrigger(){
+        if(transform.position < 0 && facingRight){
+            FlipPlayer();
+        }else if(transform.position > 0 && !facingRight){
+            FlipPlayer();
+        }
+    }
+    
+    void FlipPlayer(){
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
     }
 }
