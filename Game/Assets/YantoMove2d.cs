eccde1ec21c68b0263f5946541693030f58c12f0
mendekam <63186997+mendekam@users.noreply.github.com>
2021-02-19 22:01:45 +0700

    Update YantoMove2d.cs

--- Game/Assets/YantoMove2d.cs
+++ Game/Assets/YantoMove2d.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour
+public class YantoMove2d : MonoBehaviour
 {
     public float JumpForce;
     public float DownForce;
